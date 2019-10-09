0191992a7113dac91b53f7ed9449004300356968
Shintani Teppei <teppest1215@gmail.com>
2019-10-10 00:10:27 +0900

    first commit

---
+++ lib/grpc_required_annotator/grpc_required_annotator.rb
@@ -0,0 +1,6 @@
+require "grpc_required_annotator/version"
+
+module GrpcRequiredAnnotator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/grpc_required_annotator/version.rb
@@ -0,0 +1,3 @@
+module GrpcRequiredAnnotator
+  VERSION = "0.1.0"
+end
